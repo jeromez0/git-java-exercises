1ba31a52afb90ed894786015d73d2bc7ea9bbdc4
jeromez0 <jeromezhang1996@gmail.com>
2021-02-19 09:20:38 -0800

    Added lastlettercount class and branch

--- src/string_manipulation/LastLetterCount.java
+++ src/string_manipulation/LastLetterCount.java
@@ -1,5 +1,28 @@
 package string_manipulation;
 
 public class LastLetterCount {
-
+	
+	public LastLetterCount(String string) {
+        
+		int count=0;  
+        int index1 = 0;
+        int index2 = 0;
+        String sub1 = null;
+        String sub2 = null;
+        
+        char ch[]= new char[string.length()];     
+        
+        for( int i=0; i < string.length(); i++)  {  
+            ch[i]= string.charAt(i);  
+            if (ch[i] == 'y') {
+            	index1 = i;
+            	sub1 = string.substring(0,index1+1);
+            }
+            else if (ch[i] == 's') { 
+            	index2 = i;
+            	sub2 = string.substring(index1+1, index2+1);
+            }
+        }
+        System.out.println("Swapped string: "+sub2 + " " + sub1);
+	}
 }
