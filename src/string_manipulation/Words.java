77b8bd308d89af0fc4a5cc6fcd43523d68c8fd32
jeromez0 <jeromezhang1996@gmail.com>
2021-02-19 08:53:34 -0800

    Made the words branch and class

--- src/string_manipulation/Words.java
+++ src/string_manipulation/Words.java
@@ -2,4 +2,17 @@ package string_manipulation;
 
 public class Words {
 
+	public Words(String string) {
+        
+		int count=0;  
+        
+        char ch[]= new char[string.length()];     
+        for(int i=0;i<string.length();i++)  {  
+            ch[i]= string.charAt(i);  
+            if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )  
+                count++;  
+        }  
+        System.out.println("There are this many words in the string: " + count);  		
+	}
+	
 }
