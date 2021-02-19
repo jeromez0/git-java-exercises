577978dccac4dcd003759274bf66af84dcb89d98
jeromez0 <jeromezhang1996@gmail.com>
2021-02-19 08:20:28 -0800

    Palindromes

--- src/string_manipulation/Main.java
+++ src/string_manipulation/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String args[]) {
 		Vowels vowel = new Vowels("this is the sentence");
 		
+		Palindrome pal1 = new Palindrome("wowowowowow");
+		Palindrome pal2 = new Palindrome("awesome_possum");
 		
 	}
 	
--- src/string_manipulation/Palindrome.java
+++ src/string_manipulation/Palindrome.java
@@ -2,4 +2,16 @@ package string_manipulation;
 
 public class Palindrome {
 
+	public Palindrome(String origString) {
+        int length = origString.length();
+		
+		for(int beginIndex = 0; beginIndex < length; beginIndex++)
+        {
+            if(origString.charAt(beginIndex) != origString.charAt(length-1-beginIndex)) {
+                System.out.println("String is not a palindrome.");
+                return;
+            }
+        }
+		System.out.println("String is a palindrome");
+	}
 }
