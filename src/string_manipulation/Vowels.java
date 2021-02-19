a8f015cc294909345dbfea8bb69982575db5cdfd
jeromez0 <jeromezhang1996@gmail.com>
2021-02-19 08:05:05 -0800

    added Vowels class and Vowels branch

--- src/string_manipulation/Vowels.java
+++ src/string_manipulation/Vowels.java
@@ -2,4 +2,16 @@ package string_manipulation;
 
 public class Vowels {
 
+	int count = 0;
+	
+	public Vowels(String sentence) {
+		
+		for (int i=0; i < sentence.length(); i++) {
+			char ch = sentence.charAt(i);
+			if (ch =='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+				this.count++;				
+			}			
+		}
+		System.out.println("number of vowels in the sentence is: " + this.count);
+	}	
 }
